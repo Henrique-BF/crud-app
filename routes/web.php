1074778d38c39031d5dfbf72e5b49abf3a8cd32d
HenriqueBF <henrybf10@hotmail.com>
2022-01-05 11:26:05 -0300

    finally

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('Olá mundo.');
-});
-Route::get('/home-home', function () {
-    return view('Olá novo mundo.');
-});
-
 Route::resource('products', ProductController::class);
 Route::resource('categories', CategoryController::class);
